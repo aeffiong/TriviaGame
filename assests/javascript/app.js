52a9d43db3be3365dd13f46c720697957d2f3b73
aeffiong <aeffiong9@gmail.com>
2018-05-14 21:57:01 -0600

    got timer to work!

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -9,4 +9,28 @@ var game = {
 var numCorrect = 0;
 var numWrong = 0;
 var numUnanswered = 0;
+var timer = 120;
+var intervalId;
 
+// function to start the game
+$(document).ready(function(){
+    function start() {
+        intervalId = setInterval(decrement, 1000)
+    }
+    function decrement() {
+        timer --;
+
+        $("#timer").html("<h1> Time remaining: " + timer + " seconds</h1>");
+        if(timer === 0) {
+            clearInterval(intervalId);
+            alert("Time's Up!!")
+        }
+    
+    }
+start();
+
+// function to record answers for each question
+
+// function to change interface when time is up or user answers all questions
+    
+});
