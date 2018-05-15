10e2da260bd9e1f4feb0a795df828cbc7bbd39b5
aeffiong <aeffiong9@gmail.com>
2018-05-14 21:32:50 -0600

    started html

---
+++ assests/javascript/app.js
@@ -0,0 +1,12 @@
+// $().button('toggle')	Toggles push state. Gives the button the appearance that it has been activated. 
+// will need these for the button clicks for answers selected - might need this method
+
+// define variables
+var game = {
+
+}
+
+var numCorrect = 0;
+var numWrong = 0;
+var numUnanswered = 0;
+
